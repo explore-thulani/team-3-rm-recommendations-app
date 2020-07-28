f08e935cc1be92a0c4dbf5f84f51d2b68e3877d3
Explore Student <explore-student@ip-172-31-33-214.eu-west-1.compute.internal>
2020-07-28 14:16:35 +0000

    Updated model functions

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -32,6 +32,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 # Custom Libraries
 from utils.data_loader import load_movie_titles, load_data
 from recommenders.collaborative_based import collab_model
@@ -106,25 +108,154 @@ def main():
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
+        st.image('imgs/undraw_instant_information_6755.png', use_column_width=True)
         st.title("Solution Overview")
         st.markdown("""
-            In today’s technology driven world, recommender systems are socially and economically critical for ensuring that individuals can make appropriate choices surrounding the content they engage with on a daily basis. One application where this is especially true surrounds movie content recommendations; where intelligent algorithms can help viewers find great titles from tens of thousands of options.  
-            With this context, we as EDSA students have constructucted a recommendation algorithm based on content or collaborative filtering, capable of accurately predicting how a user will rate a movie they have not yet viewed based on their historical preferences.  
-            Providing an accurate and robust solution to this challenge has immense economic potential, with users of the system being exposed to content they would like to view or purchase - generating revenue and platform affinity.  
-            The evaluation metric for this model is Root Mean Square Error. Root Mean Square Error (RMSE) is commonly used in regression analysis and forecasting, and measures the standard deviation of the residuals arising between predicted and actual observed values for a modelling process. For our task of generating user movie ratings via recommendation algorithms.  
-
+            In today’s technology driven world, recommender systems are socially and economically critical for ensuring that individuals can make appropriate choices surrounding the content they engage with on a daily basis. One application where this is especially true surrounds movie content recommendations; where intelligent algorithms can help viewers find great titles from tens of thousands of options.
         """)
-        st.latex(r"""RMSE = \sqrt{\frac{1}{|\hat{R}|} \sum_{\hat{r}_{ui}\in \hat{R}}{(r_{ui}-\hat{r}_{ui})^2}}""")
-        st.write("""
-            
-            Where R^ is the total number of recommendations generated for users and movies, with rui and r^ui being the true and predicted ratings for user u watching movie i respectively.
+        st.write('## Problem Statment')
+        st.write('To construct a recommendation algorithm based on content or collaborative filtering, capable of accurately predicting how a user will rate a movie they have not yet viewed based on their historical preferences.')
+        st.write('## Modelling')
+        st.write('### 1. Collaborative Filtering (Singular Value Decomposition)')
+        st.write('The collaborative filtering approach builds a model based on a user’s past behaviors (for example, items previously purchased or selected and/or numerical ratings given to those items) as well as similar decisions made by other users. This model is then used to predict items (or ratings for items) that the user may have an interest in.')
+        st.write('Within collaborative filtering, there are two well-known distinct approaches:')
+        st.write('**Memory-Based:** models calculate the similarities between users / items based on user-item rating pairs.')
+        st.write('**Model-Based:** models use some sort of machine learning algorithm to estimate the ratings.')
+        st.write('### 2. Collaborative Filtering Using Cosine Similarity')
+        st.write('This model uses cosine similarity to build the recommender system. Cosine similarity is a metric used to measure how similar the documents are irrespective of their size. Mathematically, it measures the cosine of the angle between two vectors projected in a multi-dimensional space.')
+        st.write('### 3. Content-Based Filtering (Linear Regression)')
+        st.write('ontent-based recommenders treat recommendation as a user-specific classification problem and learn a classifier for the users likes and dislikes based on an items features. In this system, keywords are used to describe the items and a user profile is built to indicate the type of item this user likes.')
+        st.write('Simple linear regression is a statistical method that shows the relationship between two continuous variables. This is represented by a straight line with the equation:')
+        st.latex('$$ y = a + bx$$') 
+        st.write('where $a$ is the intercept of the line with the y-axis, and $b$ is the gradient. The independent variable ($x$) is also known as the predictor and the dependent variable ($y$) is known as the target.')
+        st.write('### 4. Collaborative Filtering (NormalPredictor, KNNBaseline, KNNBasic, KNNWithMeans, KNNWithZScore, BaselineOnly, CoClustering)')
+        st.latex(r'''
+                 For this model, the data preparation is the same as it was for the SVD model, so we will skip to the modelling.
 
-        """)
+For this model, we tried different models and used the best performing one for this part of the section. 
+
+* **Normal Predictor:**
+
+Algorithm predicting a random rating based on the distribution of the training set, which is assumed to be normal.The prediction $\hat{r}_{ui}$ is generated from a normal distribution $\mathcal{N}(\hat{\mu}, \hat{\sigma}^2)$ where $\hat{\mu}$ and $\hat{\sigma}$ are estimated from the training data using Maximum Likelihood Estimation:
+
+$$begin{split}\hat{\mu} &= \frac{1}{|R_{train}|} \sum_{r_{ui} \in R_{train}}
+r_{ui}\\\\        \hat{\sigma} &= \sqrt{\sum_{r_{ui} \in R_{train}}
+\frac{(r_{ui} - \hat{\mu})^2}{|R_{train}|}}\end{split}$$
+
+* **KNNBaseline:**
+
+A basic collaborative filtering algorithm taking into account a baseline rating.
+
+The prediction $\hat{r}_{ui}$ is set as:
+
+$$\hat{r}_{ui} = b_{ui} + \frac{ \sum\limits_{v \in N^k_i(u)}
+\text{sim}(u, v) \cdot (r_{vi} - b_{vi})} {\sum\limits_{v \in
+N^k_i(u)} \text{sim}(u, v)}$$
+
+* **KNNBasic:**
+
+A basic collaborative filtering algorithm.
+
+The prediction $\hat{r}_{ui}$ is set as:
+
+$$\hat{r}_{ui} = \frac{
+\sum\limits_{v \in N^k_i(u)} \text{sim}(u, v) \cdot r_{vi}}
+{\sum\limits_{v \in N^k_i(u)} \text{sim}(u, v)}$$
+
+
+* **KNNWithMeans:**
+
+A basic collaborative filtering algorithm, taking into account the mean ratings of each user.
+
+The prediction $\hat{r}_{ui}$ is set as:
+
+$$\hat{r}_{ui} = \mu_u + \frac{ \sum\limits_{v \in N^k_i(u)}
+\text{sim}(u, v) \cdot (r_{vi} - \mu_v)} {\sum\limits_{v \in
+N^k_i(u)} \text{sim}(u, v)}$$
+
+
+* **KNNWithZScore:** 
+
+A basic collaborative filtering algorithm, taking into account the z-score normalization of each user.
+
+The prediction $\hat{r}_{ui}$ is set as:
+
+$$ \hat{r}_{ui} = \mu_u + \sigma_u \frac{ \sum\limits_{v \in N^k_i(u)}
+\text{sim}(u, v) \cdot (r_{vi} - \mu_v) / \sigma_v} {\sum\limits_{v
+\in N^k_i(u)} \text{sim}(u, v)} $$ 
+
+* **BaselineOnly:**
+
+Algorithm predicting the baseline estimate for given user and item.
+
+$$\hat{r}_{ui} = b_{ui} = \mu + b_u + b_i$$
+
+If user $u$ is unknown, then the bias $b_u$ is assumed to be zero. The same applies for item $i$ with $b_i$.
+
+* **CoClustering:**
+
+A collaborative filtering algorithm based on co-clustering.
+
+Basically, users and items are assigned some clusters Cu, Ci, and some co-clusters Cui.
+
+The prediction $\hat{r}_{ui}$ is set as:
+
+$$\hat{r}_{ui} = \overline{C_{ui}} + (\mu_u - \overline{C_u}) + (\mu_i
+- \overline{C_i})$$
+
+
+where $\overline{C_{ui}}$ is the average rating of co-cluster $C_{ui}$, $\overline{C_u}$ is the average rating of $u$’s cluster, and $\overline{C_u}$ is the average rating of $i$’s cluster. If the user is unknown, the prediction is $\hat{r}_{ui} = \mu_i$. If the item is unknown, the prediction is $\hat{r}_{ui} = \mu_u$. If both the user and the item are unknown, the prediction is $\hat{r}_{ui} = \mu$.
+                 
+                 
+                 ''')
+        
+        # Kaggle scores
+        rmse_svd = 0.82484
+        lr_rmse = 1.06123
+        baseline_rmse = 1.24856
+        CF_cosine = 1.17123
+        baselineOnly = 0.851014
+        kNNBaseline = 0.860409
+        kNNWithMeans = 0.861267
+        kNNWithZScore = 0.861676
+        kNNBasic = 0.909070
+        coClustering = 0.919595
+        normal = 1.299894
+
+        # Compare RMSE squared values between models
+        fig, caxis = plt.subplots(figsize=(12, 6))
+        rmse_x = ['Linear Regression',
+                  'BaselineOnly', 'SVD',
+                  'Cosine Similarity',
+                  'Baseline Only',
+                  'KNN Baseline',
+                  'KNN With Means',
+                  'KNN WIth Z Score',
+                  'KNN Basic',
+                  'CoClustering',
+                  'Normal Predictor']
+        rmse_y = [lr_rmse, baseline_rmse, rmse_svd, CF_cosine, baselineOnly,
+                  kNNBaseline, kNNWithMeans, kNNWithZScore, kNNBasic,
+                  coClustering, normal]
+        ax = sns.barplot(x=rmse_x, y=rmse_y, palette='plasma_r')
+        plt.title('RMSE Values of Models', fontsize=14)
+        plt.ylabel('RMSE')
+        plt.xticks(rotation=90)
+        for p in ax.patches:
+            ax.text(p.get_x() + p.get_width()/2, p.get_y() + p.get_height(),
+                    round(p.get_height(), 2),
+                    fontsize=12, ha="center",
+                    va='bottom')
+        st.pyplot()
+        st.write('## Conclusion')
+        st.write('We were able to solve the problem statement and managed to build a model that estimates the ratings of the movie as well as make movies recommendations for the streamlit app. Based on the RMSE metric calculated for all models, we decided that the SVD is the best and would be our final model. It was definitely interesting to see that the collaborative model outperformed the content-based model because of the cold-start problem. The cold-start problem, which describes the difficulty of making recommendations when the users or the items are new, remains a great challenge for collaborative filtering. We ultimately chose the SVD model based on the RMSE metric as well as the runtime, as it only takes 15 minutes to run.')
+
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
     # ------------ EXPLORATORY DATA ANALYSIS ----------------------------
     if page_selection == "Exploratory Data Analysis":
+        st.image('imgs/undraw_business_plan_5i9d.png', use_column_width=True)
         st.title("Data overview")
         st.write(data_overview)
         image_magic('https://phhp-faculty-cantrell.sites.medinfo.ufl.edu/files/2012/07/images-mod1-big-picture-eda.gif')
--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -59,6 +59,53 @@ def data_preprocessing(subset_size):
     movies_subset = movies[:subset_size]
     return movies_subset
 
+def User_item_score(user,item):
+    a = sim_user_30_m[sim_user_30_m.index==user].values
+    b = a.squeeze().tolist()
+    c = final_movie.loc[:,item]
+    d = c[c.index.isin(b)]
+    f = d[d.notnull()]
+    avg_user = Mean.loc[Mean['userId'] == user,'rating'].values[0]
+    index = f.index.values.squeeze().tolist()
+    corr = similarity_with_movie.loc[user,index]
+    fin = pd.concat([f, corr], axis=1)
+    fin.columns = ['adg_score','correlation']
+    fin['score']=fin.apply(lambda x:x['adg_score'] * x['correlation'],axis=1)
+    nume = fin['score'].sum()
+    deno = fin['correlation'].sum()
+    final_score = avg_user + (nume/deno)
+    return final_score
+
+def User_item_score1(user):
+    Movie_seen_by_user = check.columns[check[check.index==user].notna().any()].tolist()
+    a = sim_user_30_m[sim_user_30_m.index==user].values
+    b = a.squeeze().tolist()
+    d = Movie_user[Movie_user.index.isin(b)]
+    l = ','.join(d.values)
+    Movie_seen_by_similar_users = l.split(',')
+    Movies_under_consideration = list(set(Movie_seen_by_similar_users)-set(list(map(str, Movie_seen_by_user))))
+    Movies_under_consideration = list(map(int, Movies_under_consideration))
+    score = []
+    for item in Movies_under_consideration:
+        c = final_movie.loc[:,item]
+        d = c[c.index.isin(b)]
+        f = d[d.notnull()]
+        avg_user = Mean.loc[Mean['userId'] == user,'rating'].values[0]
+        index = f.index.values.squeeze().tolist()
+        corr = similarity_with_movie.loc[user,index]
+        fin = pd.concat([f, corr], axis=1)
+        fin.columns = ['adg_score','correlation']
+        fin['score']=fin.apply(lambda x:x['adg_score'] * x['correlation'],axis=1)
+        nume = fin['score'].sum()
+        deno = fin['correlation'].sum()
+        final_score = avg_user + (nume/deno)
+        score.append(final_score)
+    data = pd.DataFrame({'movieId':Movies_under_consideration,'score':score})
+    top_5_recommendation = data.sort_values(by='score',ascending=False).head(5)
+    Movie_Name = top_5_recommendation.merge(movies, how='inner', on='movieId')
+    Movie_Names = Movie_Name.title.values.tolist()
+    return Movie_Names
+
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
 def content_model(movie_list,top_n=10):
